c744be5e43d031aa1a453c8a5abed0ba265ca8b0
Yash <153117195+Naitik4singh@users.noreply.github.com>
2024-01-05 14:44:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-# copyright 2020-22 @Mohamed Rizad
-# Telegram @riz4d
-# Instagram @riz.4d
+# copyright 2024-25 @YS_BOTZ_01
+# telegram @YS_SUPPORT_CHAT
 from pyrogram import *
 import requests as re
 from Config import *
@@ -34,7 +33,7 @@ app=Client('Temp-Mail Bot',
 email=''
 @app.on_message(filters.command('start'))
 async def start_msg(client,message):
-    await message.reply("**Hey "+message.from_user.first_name+" !!**\n @mysterymailbot is a free service that allows to generates and receive emails at a temporary address that self-destructed after a certain time elapses.\n\n**__ How It Safe's You??**__\n- Using the temporary mail allows you to completely protect your real mailbox against the loss of personal information. Your temporary e-mail address is completely anonymous. Your details: information about your person and users with whom you communicate, IP-address, e-mail address are protected and completely confidential.\n\nFurther Queris @riz4d🌚")
+    await message.reply("**Hey "+message.from_user.first_name+" !!**\n @mysterymailbot is a free service that allows to generates and receive emails at a temporary address that self-destructed after a certain time elapses.\n\n**__ How It Safe's You??**__\n- Using the temporary mail allows you to completely protect your real mailbox against the loss of personal information. Your temporary e-mail address is completely anonymous. Your details: information about your person and users with whom you communicate, IP-address, e-mail address are protected and completely confidential.\n\nFurther Queris @YS_BOTZ_01🌚")
     await message.reply("**Generate a Email Now❕**",
                         reply_markup=buttons)
 @app.on_callback_query()
@@ -93,4 +92,4 @@ async def mailbox(client,message):
         await message.edit_message_text('Session Closed✅')
 app.run()
 
-# Stay tuned for more : Instagram[@riz.4d]
+# Stay tuned for more : Telegram [https://t.me/YS_SUPPORT_CHAT]
