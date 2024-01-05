e4549340a597f6b2ea57ab44914fd2e44ab0f417
Yash <153117195+Naitik4singh@users.noreply.github.com>
2024-01-05 14:57:37 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,8 +1,8 @@
-# Fillout The variables in Config.py further queries @riz4d 0n telegram
+# Fillout The variables in Config.py further queries @YS_BOTZ_01 0n telegram
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("6796476756:AAGvgQXojSoDdWFYj_AoPO1wSgn-Jfg9-x8")
+API_ID = int(os.environ.get("28542813"))
+API_HASH = os.environ.get("02ce7c339f7776844ff4ab03da338ccd")
 
 # If You're hosting in VPS replace the following lines 9,10,11 hash symbol and replacing the approriate values on it and put the hash symbol before the lines 3,4,5
 
